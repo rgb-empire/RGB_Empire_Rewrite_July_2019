de2ab7a462f936021aaaf56c7fac4ab92e94ef40
pikipupiba <St113tt)giub41891>
2019-12-27 07:00:34 -0600

    Revert "BOOM BABY"
    This reverts commit ce302c67c03e3903e481cd37aa7e1e3b192ed3da.

--- Fixture_Parameters.h
+++ Fixture_Parameters.h
@@ -16,35 +16,35 @@
 
 
 constexpr int num_strips = 8;			// You will also need to change the Strip_Parameter array size in Fixture_Parameters in My_Enums.h
-constexpr int total_num_leds = 2400;	// TODO: make this programatic
+constexpr int total_num_leds = 2304;	// TODO: make this programatic
 
 constexpr int volts = 12;
 constexpr int milli_amps = 60000;		// Maximum power output of the connected power supply. Setting this properly will prevent brown outs!
 
 // { strip_pin, num_leds, leds_per_meter, shape, length_in_leds, width_in_leds }
-//constexpr Strip_Parameters strip_parameters[num_strips] = 
-//{
-//	{21,	288,	60,	_s_Linear,	288,	1},
-//	{19,	288,	60,	_s_Linear,	288,	1},
-//	{18,	288,	60,	_s_Linear,	288,	1},
-//	{15,	288,	60,	_s_Linear,	288,	1},
-//	{2,		288,	60,	_s_Linear,	288,	1},
-//	{0,		288,	60,	_s_Linear,	288,	1},
-//	{4,		288,	60,	_s_Linear,	288,	1},
-//	{5,		288,	60,	_s_Linear,	288,	1}
-//};
-
-constexpr Strip_Parameters strip_parameters[num_strips] =
+constexpr Strip_Parameters strip_parameters[num_strips] = 
 {
-	{13,	300,	144,	_s_Linear,	300,	1},
-	{12,	300,	144,	_s_Linear,	300,	1},
-	{26,	300,	144,	_s_Linear,	300,	1},
-	{2,		300,	144,	_s_Linear,	300,	1},
-	{0,		300,	144,	_s_Linear,	300,	1},
-	{4,		300,	144,	_s_Linear,	300,	1},
-	{16,	300,	144,	_s_Linear,	300,	1},
-	{17,	300,	144,	_s_Linear,	300,	1}
+	{21,	288,	60,	_s_Linear,	288,	1},
+	{19,	288,	60,	_s_Linear,	288,	1},
+	{18,	288,	60,	_s_Linear,	288,	1},
+	{15,	288,	60,	_s_Linear,	288,	1},
+	{2,		288,	60,	_s_Linear,	288,	1},
+	{0,		288,	60,	_s_Linear,	288,	1},
+	{4,		288,	60,	_s_Linear,	288,	1},
+	{5,		288,	60,	_s_Linear,	288,	1}
 };
+
+//constexpr Strip_Parameters strip_parameters[num_strips] =
+//{
+//	{13,	300,	144,	_s_Linear,	300,	1},
+//	{12,	300,	144,	_s_Linear,	300,	1},
+//	{26,	300,	144,	_s_Linear,	300,	1},
+//	{2,		300,	144,	_s_Linear,	300,	1},
+//	{0,		300,	144,	_s_Linear,	300,	1},
+//	{4,		300,	144,	_s_Linear,	300,	1},
+//	{16,	300,	144,	_s_Linear,	300,	1},
+//	{17,	300,	144,	_s_Linear,	300,	1}
+//};
 
 // { num_strips, total_num_leds, volts, milliamps, strip_parameters[] }
 constexpr Fixture_Parameters fixture_parameters =
